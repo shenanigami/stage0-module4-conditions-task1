58413fe7697aea99fb51e3ff4acdc49a6056916c
shenanigami <38479116+shenanigami@users.noreply.github.com>
2023-08-28 21:57:15 +0600

    First batch of changes

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if(false) {
+            System.out.println("This will never be printed");
+        } else {
+            System.out.println(false);
+        }
     }
 }
